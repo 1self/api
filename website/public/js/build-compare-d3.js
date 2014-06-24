5fea5cf24bdcdabfcd11ecab46aa3b31403a1f08
devikadeshmukh <ddeshmukh@equalexperts.com>
2014-06-24 16:56:49 +0530

    Completed visualisation for comparison of build events

--- website/public/js/build-compare-d3.js
+++ website/public/js/build-compare-d3.js
@@ -8,8 +8,8 @@ window.qd.plotComparison = function(divId, myBuildEvents, withBuildEvents) {
 		x = d3.scale.ordinal().rangeRoundBands([0, w - p[1] - p[3]]),
 		xLinear = d3.scale.linear().range([0, w - p[1] - p[3]]);
 	var y = d3.scale.linear().range([0, h - p[0] - p[2]]),
-	parse = d3.time.format("%m/%d/%Y").parse,
-	format = d3.time.format("%d");
+		parse = d3.time.format("%m/%d/%Y").parse,
+		format = d3.time.format("%d");
 	formatMonth = d3.time.format("%b");
 
 	var svg = d3.select(divId).append("svg:svg")
@@ -18,120 +18,128 @@ window.qd.plotComparison = function(divId, myBuildEvents, withBuildEvents) {
 		.append("svg:g")
 		.attr("transform", "translate(" + p[3] + "," + (h - p[2]) + ")");
 
-	var myBuildEventsGroupByStatus = ["failed", "passed"].map(function(status) {
-		return myBuildEvents.map(function(d) {
-			return {
-				x: parse(d.date),
-				y: +d[status]
-			};
+
+	var _groupByStatus = function(eventsMap) {
+		return ["failed", "passed"].map(function(status) {
+			return eventsMap.map(function(d) {
+				return {
+					x: parse(d.date),
+					y: +d[status]
+				};
+			});
 		});
-	});
+	};
+
+	var _yMax = function(arrayOfArrays) {
+		return d3.max(arrayOfArrays, function(array) {
+			return d3.max(array, function(d) {
+				return d.y;
+			});
+		});
+	};
+	var _plotLineGraph = function(color, isDashedLine, dataArray) {
+		var dashArrayValue = isDashedLine === true ? "2,2" : "0,0";
+
+		var lineGraph = d3.svg.line()
+			.x(function(d, i) {
+				return xLinear(i);
+			})
+			.y(function(d, i) {
+				var curval = +d.y;
+				return -y(curval);
+			})
+			.interpolate("monotone");
+
+		svg.append("path")
+			.attr("class", "average")
+			.attr("d", lineGraph(dataArray))
+			.style("fill", "none")
+			.style("stroke", color)
+			.style("stroke-dasharray", dashArrayValue)
+			.style("stroke-width", 2);
+
+	};
+	var _createAxesAndLabels = function() {
+		//    Add a label per date
+		var label = svg.selectAll("text.month")
+			.data(x.domain())
+			.enter().append("svg:text")
+			.attr("x", function(d) {
+				return x(d) + x.rangeBand() / 2;
+			})
+			.attr("y", 6)
+			.attr("text-anchor", "middle")
+			.attr("dy", ".71em")
+			.text(function(d, i) {
+				return (i % 7) ? null : formatMonth(d);
+			});
+
+		var filterFormat = format;
+		if (w < 800) {
+			filterFormat = function(d, i) {
+				return (i % 7) ? null : format(d);
+			};
+		}
+
+		var label = svg.selectAll("text.day")
+			.data(x.domain())
+			.enter().append("svg:text")
+			.attr("x", function(d) {
+				return x(d) + x.rangeBand() / 2;
+			})
+			.attr("y", 19)
+			.attr("text-anchor", "middle")
+			.attr("dy", ".71em")
+			.text(filterFormat);
+
+		// Add y-axis rules.
+		var rule = svg.selectAll("g.rule")
+			.data(y.ticks(5))
+			.enter().append("svg:g")
+			.attr("class", "rule")
+			.attr("transform", function(d) {
+				return "translate(0," + -y(d) + ")";
+			});
+
+		rule.append("svg:line")
+			.attr("x2", w - p[1] - p[3])
+			.style("stroke", function(d) {
+				return d ? "#fff" : "#000";
+			})
+			.style("stroke-opacity", function(d) {
+				return d ? .7 : null;
+			});
+
+		rule.append("svg:text")
+			.attr("x", w - p[1] - p[3] + 6)
+			.attr("dy", ".35em")
+			.text(d3.format(",d"));
+	};
+	var myBuildEventsGroupByStatus = _groupByStatus(myBuildEvents);
 	var myFailedBuildEvents = myBuildEventsGroupByStatus[0];
 
 	x.domain(myFailedBuildEvents.map(function(d) {
 		return d.x;
 	}));
 	xLinear.domain([0, myFailedBuildEvents.length]);
-
-	var maxFailedValue = d3.max(myFailedBuildEvents, function(d) { return d.y; });
 	var myPassedBuildEvents = myBuildEventsGroupByStatus[1];
-	var maxPassedValue = d3.max(myPassedBuildEvents, function(d) { return d.y; });
+	var withBuildEventsGroupByStatus = _groupByStatus(withBuildEvents);
+	var withFailedBuildEvents = withBuildEventsGroupByStatus[0];
+	var withPassedBuildEvents = withBuildEventsGroupByStatus[1];
+	y.domain([0, _yMax([myPassedBuildEvents, myFailedBuildEvents, withPassedBuildEvents, withFailedBuildEvents])]);
 
-	y.domain([0, d3.max([maxFailedValue, maxPassedValue])]);
-
-	//    Add a label per date
-	var label = svg.selectAll("text.month")
-		.data(x.domain())
-		.enter().append("svg:text")
-		.attr("x", function(d) {
-			return x(d) + x.rangeBand() / 2;
-		})
-		.attr("y", 6)
-		.attr("text-anchor", "middle")
-		.attr("dy", ".71em")
-		.text(function(d, i) {
-			return (i % 7) ? null : formatMonth(d);
-		});
-
-	var filterFormat = format;
-	if (w < 800) {
-		filterFormat = function(d, i) {
-			return (i % 7) ? null : format(d);
-		};
-	}
-
-	var label = svg.selectAll("text.day")
-		.data(x.domain())
-		.enter().append("svg:text")
-		.attr("x", function(d) {
-			return x(d) + x.rangeBand() / 2;
-		})
-		.attr("y", 19)
-		.attr("text-anchor", "middle")
-		.attr("dy", ".71em")
-		.text(filterFormat);
-
-	// Add y-axis rules.
-	var rule = svg.selectAll("g.rule")
-		.data(y.ticks(5))
-		.enter().append("svg:g")
-		.attr("class", "rule")
-		.attr("transform", function(d) {
-			return "translate(0," + -y(d) + ")";
-		});
-
-	rule.append("svg:line")
-		.attr("x2", w - p[1] - p[3])
-		.style("stroke", function(d) {
-			return d ? "#fff" : "#000";
-		})
-		.style("stroke-opacity", function(d) {
-			return d ? .7 : null;
-		});
-
-	rule.append("svg:text")
-		.attr("x", w - p[1] - p[3] + 6)
-		.attr("dy", ".35em")
-		.text(d3.format(",d"));
-
-		console.info("Rule: ", rule);
+	_createAxesAndLabels();
 
 	//My Successful Builds:
-	var myPassedBuilds = d3.svg.line()
-		.x(function(d, i) {
-			return xLinear(i);
-		})
-		.y(function(d, i) {
-			var curval = +d.y;
-			return -y(curval);
-		})
-		.interpolate("monotone");
-
-	svg.append("path")
-		.attr("class", "average")
-		.attr("d", myPassedBuilds(myPassedBuildEvents))
-		.style("fill", "none")
-		.style("stroke", "blue")
-		.style("stroke-width", 2);
+	_plotLineGraph("#2e4174", false, myPassedBuildEvents);
 
 	//My Failed Builds:
-	var myFailedBuilds = d3.svg.line()
-		.x(function(d, i) {
-			return xLinear(i);
-		})
-		.y(function(d, i) {
-			var curval = +d.y;
-			console.info("curval = , y(curval) =", curval, -y(curval));
-			return -y(curval); 
-		})
-		.interpolate("monotone");
+	_plotLineGraph("#ED1C25", false, myFailedBuildEvents);
 
-	svg.append("path")
-		.attr("class", "average")
-		.attr("d", myFailedBuilds(myFailedBuildEvents))
-		.style("fill", "none")
-		.style("stroke", "red")
-		.style("stroke-width", 2);
+	//With Successful Builds:
+	_plotLineGraph("#73b9e6", true, withPassedBuildEvents);
 
+	//With Failed Builds:
+	_plotLineGraph("#F2555C", true, withFailedBuildEvents);
 
 };
