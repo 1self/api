df2af6ca2f03252cdfc506aee0fcceb00c71e5df
Chinmay Naik <cnaik@equalexperts.com>
2014-07-16 11:49:49 +0530

    - removed streamId and readToken from compare page.

--- website/public/js/compare.js
+++ website/public/js/compare.js
@@ -1,38 +1,3 @@
-var updateLocalStorage = function() {
-	window.localStorage.streamId = $('#my-stream-id').val();
-	window.localStorage.readToken = $('#my-read-token').val();
-	window.localStorage.theirStreamId = $('#their-stream-id').val();
-	window.localStorage.theirReadToken = $('#their-read-token').val();
-};
-
-$('#auth-save-compare').click(function() {
-	updateLocalStorage();
-	window.qd.plotComparisonGraphs();
-});
-
 $(document).ready(function() {
-	var myDetailsAreAvailable = function() {
-		return window.localStorage.streamId && window.localStorage.readToken;
-	}
-	var populateMyTextboxes = function() {
-		$('#my-stream-id').val(window.localStorage.streamId);
-		$('#my-read-token').val(window.localStorage.readToken);
-	}
-	var theirDetailsAreAvailable = function() {
-		return window.localStorage.theirStreamId && window.localStorage.theirReadToken;
-	}
-	var populateTheirTextboxes = function() {
-		$('#their-stream-id').val(window.localStorage.theirStreamId);
-		$('#their-read-token').val(window.localStorage.theirReadToken);
-	}
-
-	if (myDetailsAreAvailable()) {
-		populateMyTextboxes();
-	}
-	if (theirDetailsAreAvailable()) {
-		populateTheirTextboxes();
-	}
-	if (myDetailsAreAvailable() && theirDetailsAreAvailable()) {
-		window.qd.plotComparisonGraphs();
-	}
+	window.qd.plotComparisonGraphs();
 });
