3bf05a78b7089e7b81b8f49c2e0de0748847c73b
devikadeshmukh <ddeshmukh@equalexperts.com>
2014-07-22 11:59:09 +0530

    - Added validation to the link streamId modal

--- website/public/js/dashboard.js
+++ website/public/js/dashboard.js
@@ -21,6 +21,18 @@ $("#builds-x").ready(function() {
 	});
 });
 
+var validateStreamDetails = function() {
+	$("#stream-error").hide();
+	var streamId = $("#streamId").val();
+	var readToken = $("#readToken").val();
+	if (streamId.trim() && readToken.trim()) {
+		return true;
+	}
+	$("#stream-error").show();
+
+	return false;
+};
+
 $(document).ready(function() {
 	window.qd.plotGraphs(dashboardGraphs);
 });
