0b8121843d4cc06c935caef5160509dc01c0490c
Chinmay Naik <cnaik@equalexperts.com>
2014-07-30 16:37:13 +0530

    - Removed the border of iframe and adjusted the height

--- website/public/js/globe.js
+++ website/public/js/globe.js
@@ -1,6 +1,6 @@
 var iframeWidth = $("#qd-globe").parent().width();
 
 function resizeIframe(obj) {
-	obj.style.height = obj.contentWindow.document.body.scrollHeight - 20 + 'px';
+	obj.style.height = obj.contentWindow.document.body.scrollHeight - 25 + 'px';
 	obj.style.width = iframeWidth + 'px';
 }
