d4017b4de8f9b498a4b07868db607d618a7b3f32
Anil Wadghule <awadghule@equalexperts.com>
2014-08-26 22:43:41 +0530

    Fix warnings

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var requestModule = require('request');
-var cheerio = require('cheerio');
 var express = require("express");
 var moment = require("moment");
 var url = require('url');
@@ -464,7 +463,7 @@ var getBuildEventsFromPlatform = function(params) {
                     value: 0
                 }];
                 var buildsByDay = generateDatesFor(defaultBuildValues);
-                for (date in result) {
+                for (var date in result) {
                     if (buildsByDay[date] !== undefined) {
                         buildsByDay[date].passed = result[date].passed;
                         buildsByDay[date].failed = result[date].failed;
@@ -1291,7 +1290,7 @@ var getGithubPushEventCountForCompare = function(params) {
     };
 
     function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             var result = JSON.parse(body);
             var defaultGithubPushEventsForCompare = [{
                 key: "my",
@@ -1303,10 +1302,10 @@ var getGithubPushEventCountForCompare = function(params) {
             var githubPushEventsForCompare = generateDatesFor(defaultGithubPushEventsForCompare);
             for (var date in result) {
                 if (githubPushEventsForCompare[date] !== undefined) {
-                    if (result[date].myGithubPushEventCount == undefined) {
+                    if (result[date].myGithubPushEventCount === undefined) {
                         result[date].myGithubPushEventCount = 0;
                     }
-                    if (result[date].theirGithubPushEventCount == undefined) {
+                    if (result[date].theirGithubPushEventCount === undefined) {
                         result[date].theirGithubPushEventCount = 0;
                     }
                     githubPushEventsForCompare[date].my = result[date].myGithubPushEventCount;
@@ -1417,7 +1416,7 @@ var getIdeActivityDurationForCompare = function(params) {
     };
 
     function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             var result = JSON.parse(body);
             var defaultIdeActivityDurationForCompare = [{
                 key: "my",
@@ -1429,10 +1428,10 @@ var getIdeActivityDurationForCompare = function(params) {
             var ideActivityDurationForCompare = generateDatesFor(defaultIdeActivityDurationForCompare);
             for (var date in result) {
                 if (ideActivityDurationForCompare[date] !== undefined) {
-                    if (result[date].myIdeActivityDuration == undefined) {
+                    if (result[date].myIdeActivityDuration === undefined) {
                         result[date].myIdeActivityDuration = 0;
                     }
-                    if (result[date].restOfTheWorldIdeActivityDuration == undefined) {
+                    if (result[date].restOfTheWorldIdeActivityDuration === undefined) {
                         result[date].restOfTheWorldIdeActivityDuration = 0;
                     }
                     ideActivityDurationForCompare[date].my = convertMillisToMinutes(result[date].myIdeActivityDuration);
@@ -1560,7 +1559,7 @@ var getDailyGithubPushEventsCount = function(streamid) {
     };
 
     function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             var result = JSON.parse(body);
             result = result[0];
             var defaultEventValues = [{
@@ -1656,7 +1655,7 @@ var getMyActiveDuration = function(params) {
     };
 
     function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             var result = JSON.parse(body);
             if (_.isEmpty(result)) {
                 deferred.resolve([]);
@@ -1763,10 +1762,10 @@ var correlateGithubPushesAndIDEActivity = function(params) {
                 deferred.resolve([]);
             } else {
                 for (var date in result) {
-                    if (result[date].activeTimeInMillis == undefined) {
+                    if (result[date].activeTimeInMillis === undefined) {
                         result[date].activeTimeInMillis = 0;
                     }
-                    if (result[date].githubPushEventCount == undefined) {
+                    if (result[date].githubPushEventCount === undefined) {
                         result[date].githubPushEventCount = 0;
                     }
                     result[date].activeTimeInMinutes = convertMillisToMinutes(result[date].activeTimeInMillis);
@@ -1860,7 +1859,7 @@ app.get('/stream/:id', function(req, res) {
         qdDb.collection('stream').find(spec, function(err, docs) {
             docs.toArray(function(err, streamArray) {
                 var stream = streamArray[0] || {};
-                if (stream.readToken != readToken) {
+                if (stream.readToken !== readToken) {
                     res.status(404).send("stream not found");
                 } else {
                     var response = {
@@ -1897,7 +1896,7 @@ app.get('/eventsCount', function(req, res) {
 
 app.get('/:ip', function(req, res) {
     requestModule('http://freegeoip.net/json/' + req.params.ip, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             res.send(body);
         }
     });
@@ -1945,7 +1944,7 @@ app.get('/live/devbuild/:durationMins', function(req, res) {
     };
 
     function callback(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             var info = JSON.parse(body);
             res.send(info);
         } else {
