e1fd340afac7892393e99da147e377066736844f
Chinmay Naik <cnaik@equalexperts.com>
2014-09-12 16:12:43 +0530

    -- another template for bar graphs

--- website/public/js/realTimeNoise.js
+++ website/public/js/realTimeNoise.js
@@ -1,95 +1,132 @@
-var plotNoiseGraph = function() {
+var plotNoiseGraph = function(noiseData) {
 	$('#noise-history').empty();
 	var margin = {
 		top: 20,
-		right: 20,
+		right: 0,
 		bottom: 30,
 		left: 30
 	};
-	var width = $('#noise-history').width();
-	var height = width / 1.61;
+	var w = $('#noise-history').width();
+	var h = w / 1.61;
 
-	 var t = -1;
-    var n = 40;
-    var v = 0;
-    var data = d3.range(n).map(next);
-	
-    function next () {
-        return {
-            time: ++t,
-            value: v = Math.floor(Math.random()*20)
-        };
-    };
-	 
-    var x = d3.scale.linear()
-        .domain([0, n - 1])
-        .range([0, width]);
-	 
-    var y = d3.scale.linear()
-        .domain([0, 20])
-        .range([height, 0]);
-	 
-    var line = d3.svg.line()
-        .x(function(d, i) { console.log(d.time); return x(d.time); })
-        .y(function(d, i) { return y(d.value); });
-		 
-    var svg = d3.select("#noise-history").append("svg")
-        .attr("width", width - margin.left - margin.right)
-        .attr("height", height + margin.top + margin.bottom);
-	
-    var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-		
-    var graph = g.append("svg")
-        .attr("width", width - margin.left - margin.right)
-        .attr("height", height + margin.top + margin.bottom);	
-	 
-    var xAxis = d3.svg.axis().scale(x).orient("bottom");
-    var axis = graph.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-	 
-    g.append("g")
-        .attr("class", "y axis")
-        .call(d3.svg.axis().scale(y).orient("left"));
-	 
-	var path = graph.append("g")
-		.append("path")
-		.data([data])
-		.attr("class", "realTimeLine")
-		.attr("d", line);
-		
-    tick();
-		 
-    function tick() 
-	{ 
-        // push a new data point onto the back
-        data.push(next());
+	var dataset = [{
+		key: 0,
+		value: 5
+	}, {
+		key: 1,
+		value: 10
+	}, {
+		key: 2,
+		value: 13
+	}, {
+		key: 3,
+		value: 19
+	}, {
+		key: 4,
+		value: 21
+	}, {
+		key: 5,
+		value: 25
+	}];
 
-        // update domain
-        x.domain([t - n, t]);
-	
-        // redraw path, shift path left
-        path
-            .attr("d", line)
-            .attr("transform", null)
-            .transition()
-            .duration(500)
-            .ease("linear")
-            .attr("transform", "translate(" + t - 1 + ")")
-            .each("end", tick);
-	
-        // shift axis left
-        axis
-            .transition()
-            .duration(500)
-            .ease("linear")
-            .call(d3.svg.axis().scale(x).orient("bottom"));
-	 
-        // pop the old data point off the front
-        data.shift();	 
-    };
+	var xScale = d3.scale.ordinal()
+		.domain(d3.range(dataset.length))
+		.rangeRoundBands([0, w], 0.05);
+
+	var yScale = d3.scale.linear()
+		.domain([0, d3.max(dataset, function(d) {
+			return d.value;
+		})])
+		.range([0, h]);
+
+	//Define key function, to be used when binding data
+	var key = function(d) {
+		return d.key;
+	};
+
+	//Create SVG element
+	var svg = d3.select("#noise-history")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h);
+
+	//Create bars
+	svg.selectAll("rect")
+		.data(dataset, key)
+		.enter()
+		.append("rect")
+		.attr("x", function(d, i) {
+			return xScale(i);
+		})
+		.attr("y", function(d) {
+			return h - yScale(d.value);
+		})
+		.attr("width", xScale.rangeBand())
+		.attr("height", function(d) {
+			return yScale(d.value);
+		})
+		.attr("fill", function(d) {
+			return "rgb(0, 0, " + (d.value * 10) + ")";
+		});
+
+	//On click, update with new data			
+	setInterval(function() {
+		var maxValue = 25;
+		var newNumber = Math.floor(Math.random() * maxValue);
+		var lastKeyValue = dataset[dataset.length - 1].key;
+		console.log(lastKeyValue);
+		dataset.push({
+			key: lastKeyValue + 1,
+			value: newNumber
+		});
+		dataset.shift(); //Remove one value from dataset
+
+		//Update scale domains
+		xScale.domain(d3.range(dataset.length));
+		yScale.domain([0, d3.max(dataset, function(d) {
+			return d.value;
+		})]);
+
+		//Select…
+		var bars = svg.selectAll("rect")
+			.data(dataset, key);
+
+		//Enter…
+		bars.enter()
+			.append("rect")
+			.attr("x", w)
+			.attr("y", function(d) {
+				return h - yScale(d.value);
+			})
+			.attr("width", xScale.rangeBand())
+			.attr("height", function(d) {
+				return yScale(d.value);
+			})
+			.attr("fill", function(d) {
+				return "rgb(0, 0, " + (d.value * 10) + ")";
+			});
+
+		//Update…
+		bars.transition()
+			.duration(500)
+			.attr("x", function(d, i) {
+				return xScale(i);
+			})
+			.attr("y", function(d) {
+				return h - yScale(d.value);
+			})
+			.attr("width", xScale.rangeBand())
+			.attr("height", function(d) {
+				return yScale(d.value);
+			});
+
+		//Exit…
+		bars.exit()
+			.transition()
+			.duration(500)
+			.attr("x", -xScale.rangeBand())
+			.remove();
+	}, 1000);
 };
 
 $(document).ready(function() {
@@ -99,7 +136,7 @@ $(document).ready(function() {
 	socket.on('snapshot', function(data) {
 		noiseData = data;
 		console.info("snapshot noise Data : " + JSON.stringify(noiseData));
-		plotNoiseGraph();
+		plotNoiseGraph(noiseData);
 	});
 
 	socket.on('realTimeData', function(data) {
