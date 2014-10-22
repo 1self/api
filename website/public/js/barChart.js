edd532a623b32bbd441276033aadd40d6d3d216c
Anil Wadghule <awadghule@equalexperts.com>
2014-10-22 16:39:22 +0530

    — Added the url to the visualisation — Removed the labels on the axes

--- website/public/js/barChart.js
+++ website/public/js/barChart.js
@@ -119,25 +119,11 @@ var plotBarChart = function (divId, events, fromTime, tillTime) {
         svg.append('g')
             .attr('class', 'x axis')
             .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
-            .call(xAxis)
-            .append("text")
-            .attr("class", "label")
-            .attr("x", width - margin.left - margin.right)
-            .attr("y", -30)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Event "+ operation);
+            .call(xAxis);
 
         svg.append('g')
             .attr('class', 'y axis')
-            .call(yAxis)
-            .append("text")
-            .attr("class", "label")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Date");
+            .call(yAxis);
     }, 1000);
 };
 
