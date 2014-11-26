0164cec3f59e10a99e950ace301c4a203c4e920c
Anil Wadghule <anildigital@gmail.com>
2014-11-26 09:26:57 +0000

    Always redirect to dashboard when user logs in from signup page.

--- quantifieddevRoutes.js
+++ quantifieddevRoutes.js
@@ -38,7 +38,7 @@ module.exports = function (app) {
 
     app.get("/signup", function (req, res) {
         if (!(_.isEmpty(req.param('streamId')))){
-            req.session.redirectUrl = req.originalUrl + "?streamId=" + req.param('streamId');
+            req.session.redirectUrl = "/dashboard" + "?streamId=" + req.param('streamId');
         }
         res.render('signup');
     });
