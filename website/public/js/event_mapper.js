068acdab42e91164a94a28b6f4f613ad9fbec04e
Devaroop <devaroop123@yahoo.co.in>
2014-12-02 11:53:23 +0530

    adding hardcoded events

--- website/public/js/event_mapper.js
+++ website/public/js/event_mapper.js
@@ -67,13 +67,141 @@ var preDefinedOsEvents = [
                  unitOfMeasurement: ""
                 }),
 
-    new OsEvent({name: "Exercising", 
+    new OsEvent({name: "Coding", 
                  objectTags: ["self"],
-                 actionTags: ["exercise"],
+                 actionTags: ["code"],
                  interestingProperty: "duration",
                  operation: "sum(duration)",
                  unitOfMeasurement: "milliseconds"
-                })
+                }),
+
+    new OsEvent({name: "Commuting", 
+                 objectTags: ["self"],
+                 actionTags: ["commute"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Cooking", 
+                 objectTags: ["self"],
+                 actionTags: ["cook"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Meditating", 
+                 objectTags: ["self"],
+                 actionTags: ["meditate"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Meeting", 
+                 objectTags: ["self"],
+                 actionTags: ["meet"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Partying", 
+                 objectTags: ["self"],
+                 actionTags: ["party"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Playing Instrument", 
+                 objectTags: ["instrument"],
+                 actionTags: ["play"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Playing Games", 
+                 objectTags: ["computer", "games"],
+                 actionTags: ["play"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Reading", 
+                 objectTags: ["text"],
+                 actionTags: ["read"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Sitting", 
+                 objectTags: ["self"],
+                 actionTags: ["sit"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Sleeping", 
+                 objectTags: ["self"],
+                 actionTags: ["sleep"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Standing", 
+                 objectTags: ["self"],
+                 actionTags: ["stand"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Studying", 
+                 objectTags: ["self"],
+                 actionTags: ["study"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Tooth Brushing", 
+                 objectTags: ["teeth"],
+                 actionTags: ["floss"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "TV Watching", 
+                 objectTags: ["tv"],
+                 actionTags: ["watch"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Working", 
+                 objectTags: ["self"],
+                 actionTags: ["work"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                }),
+
+    new OsEvent({name: "Writing", 
+                 objectTags: ["text"],
+                 actionTags: ["write"],
+                 interestingProperty: "duration",
+                 operation: "sum(duration)",
+                 unitOfMeasurement: "milliseconds"
+                });
 ];
 
 var os_event_lookup = new OsEventLookup(preDefinedOsEvents);
