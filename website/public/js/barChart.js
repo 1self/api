15b5c7fe1913a323bc82a1440cf0c7453b2b1106
Ed Sykes <ed.sykes@gmail.com>
2015-02-21 03:59:56 +0000

    refactoring

--- website/public/js/barChart.js
+++ website/public/js/barChart.js
@@ -13,38 +13,6 @@ var addUnits = function(svg, units, graphHeight) {
 
 
 var addYaxis = function(svg, yAxis){
- // var filter = svg.append("svg:defs")
- //            .append("filter")
- //            .attr("x", "-10%")
- //            .attr("y", "-100%")
- //            .attr("id", "nowline-drop-shadow")
- //            .attr("height", "200%")
- //            .attr("width", "180%");
-
- //        var comptransf = filter.append("feComponentTransfer");
- //        comptransf.append("feFuncA")
- //            .attr("type", "linear")
- //            .attr("slope", "10.7")
- //        comptransf.append("feFuncR")
- //            .attr("type", "linear")
- //            .attr("slope", "-10.7")
- //        comptransf.append("feFuncG")
- //            .attr("type", "linear")
- //            .attr("slope", "0")
- //        comptransf.append("feFuncB")
- //            .attr("type", "linear")
- //            .attr("slope", "0")
-
- //        var blur = filter.append("feGaussianBlur")
- //            .attr("stdDeviation", 0);
- //        blur.transition().delay(1500).attr("stdDeviation", 5);
-
- //        var feMerge = filter.append("feMerge");
-
- //        feMerge.append("feMergeNode");
- //        feMerge.append("feMergeNode")
- //            .attr("in", "SourceGraphic");
-
     svg.append('g')
             .attr('class', 'y axis')
             .call(yAxis)
@@ -115,13 +83,6 @@ var addNowLine = function(svg, x, graphHeight) {
         .attr('fill', 'none')
         .style("filter", "url(#overlay-shadow)");
 
-    // var lineGraph = svg.append('path')
-    //     .attr('d', lineFunction(lineData))
-    //     .attr('stroke', 'rgba(255, 255, 255, 0.8')
-    //     .attr('stroke-dasharray', [5, 5])
-    //     .attr('stroke-width', 1)
-    //     .attr('fill', 'none');
-
     var nowLabel = svg.append('text')
         .attr('text-anchor', 'end')
         .attr('x', xGraphNowLocation - 3)
@@ -130,70 +91,8 @@ var addNowLine = function(svg, x, graphHeight) {
         .text('now');
 };
 
-charts.plotBarChart = function(divId, events, fromTime, tillTime, units) {
-    events = _.sortBy(events, function(e) {
-        return e.date;
-    })
-    setTimeout(function() {
-        $(divId).empty();
-        var margin = {
-            top: 20,
-            right: 0,
-            bottom: 0,
-            left: 0
-        };
-        var width = window.innerWidth;
-        var height = width / 1.61;
-        var weekAgo = new Date(moment().subtract('days', 6).format("MM/DD/YYYY"));
-        var tomorrow = new Date(moment().add('day', 1).format("MM/DD/YYYY"));
-        var xWidth = width / 7;
-        var x = d3.time.scale()
-            .domain([weekAgo, tomorrow])
-            .rangeRound([0, width - margin.left - margin.right])
-            .nice();
-
-        var maxDataValue = d3.max(events, function(d) {
-            return d.value;
-        });
-
-        var innerGraphHeight = height - margin.top - margin.bottom;
-
-        var y = d3.scale.linear()
-            .domain([0, maxDataValue])
-            .range([innerGraphHeight, 0])
-            .nice(4);
-
-        var xAxis = d3.svg.axis()
-            .scale(x);
-
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient('right')
-            .ticks(5);
-
-        var svg = d3.select(divId).append('svg')
-            .attr('class', 'chart')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-            .attr('id', 'bars')
-            .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
-        var tipText = function(d) {
-            return "<strong>" + operation + " " + d.value +
-                "</strong> <span style='color:lightgrey'> on " + moment(d.date).format("ddd MMM DD") + "</span>";
-        };
-        var tooltipDivForMobile = d3.select("body").append("div")
-            .attr("class", "tooltip")
-            .style("opacity", 0);
-        var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-                return tipText(d);
-            });
-        svg.call(tip);
-        var gradient = svg.append("svg:defs")
+var addGradients = function(svg){
+    var gradient = svg.append("svg:defs")
             .append("svg:linearGradient")
             .attr("id", "gradient")
             .attr("x1", "0%")
@@ -230,8 +129,10 @@ charts.plotBarChart = function(divId, events, fromTime, tillTime, units) {
             .attr("offset", "100%")
             .attr("stop-color", "#fff")
             .attr("stop-opacity", 0.7);
+}
 
-        var filter = svg.append("svg:defs")
+var addFilter = function(svg){
+    var filter = svg.append("svg:defs")
             .append("filter")
             .attr("x", "-70%")
             .attr("y", "-300%")
@@ -262,16 +163,80 @@ charts.plotBarChart = function(divId, events, fromTime, tillTime, units) {
         feMerge.append("feMergeNode");
         feMerge.append("feMergeNode")
             .attr("in", "SourceGraphic");
+}
 
-        var chart = svg.selectAll('.chart');
+var attachClickHighlight = function(chart, data){
+    chart.on("click", function(d) {
+        console.log('clicked');
+                var bars = svg.selectAll('.bar');
+                bars.style("stroke", function(data) {
 
-        chart.data(events)
-            .enter()
-            .append('rect')
-            .attr('class', 'bar')
-            .style("fill", "url(#gradient)")
-            .style("stroke", "rgba(255,255,255,0.7)")
-            .on("click", function(d) {
+                        if (d === data) {
+                            return null;
+                        } else {
+                            return "rgba(255,255,255,0.7)";
+                        }
+                    })
+                    .style("filter", function(data) {
+                        if (d === data) {
+                            return "url(#drop-shadow)";
+                        }
+                    })
+                    .style("stroke-width", function(data) {
+                        if (d === data) {
+                            return "3px";
+                        } else {
+                            return "1px";
+                        }
+                    })
+                    .style("fill", function(data) {
+                        if (d === data) {
+                            return "url(#gradient_highlight)";
+                        } else {
+                            return "url(#gradient)"
+                        }
+                    })
+                    .style("background-color", function(data) {
+                        if (d === data) {
+                            return "rgba(0, 0, 0, 0.22)";
+                        }
+                    });
+                $(".addCommentButton").show();
+                $("#date").html(moment(d.date).format("DD/MM/YY dddd"));
+                $("#eventValue").html(getDataPointDescription(d.value));
+
+                setGraphUrl(d.date);
+                charts.selectedDate = moment(d.date).format("YYYY-MM-DD");
+                charts.showComments();
+
+                // in order to make the shadow work correctly the highlighted
+                // bar needs to be drawn last. This makes the <g> element that
+                // is created last in the list of elements which makes it on 
+                // top of everything else
+                for (var i = bars.data().length - 1; i >= 0; i--) {
+                    if (d === bars.data()[i]) {
+                        var bar = bars[0][i];
+                        $('#bars').append(bar);
+                    }
+                }
+
+                // When the selected bar is the first one, it draws over the y axis.
+                // Therefore, we must redraw the y axis.
+                d3.select('.y.axis').remove();
+                svg.append('g')
+                    .attr('class', 'y axis')
+                    .call(yAxis);
+
+                blur.attr("stdDeviation", 0);
+                blur.transition().attr("stdDeviation", 5);
+            })
+}
+
+var attachClickHighlightAnimated = function(chart, data){
+    console.log('attaching');
+    console.log(chart);
+    chart.on("click", function(d) {
+        console.log('clicked');
                 var bars = svg.selectAll('.bar');
                 bars
                     .transition()
@@ -336,35 +301,110 @@ charts.plotBarChart = function(divId, events, fromTime, tillTime, units) {
                 blur.attr("stdDeviation", 0);
                 blur.transition().attr("stdDeviation", 5);
             })
+}
 
-        .attr('x', function(d) {
-            return x(new Date(d.date));
+var setStartToCollapsed = function(chart, xScale, height, xWidth){
+    chart.attr('x', function(d) {
+            return xScale(new Date(d.date));
         })
-            .attr('y', height)
-            .attr('width', xWidth)
-            .attr('height', 0)
-            .transition()
+    .attr('y', height)
+    .attr('width', xWidth)
+    .attr('height', 0);
+}
+
+var addAnimation = function(bars){
+    var result  = bars.transition()
             .delay(
                 function(d, i) {
                     return i * 130 + 30;
                 })
             .ease('cubic')
-            .duration(240)
-            .attr('height', function(d) {
-                return innerGraphHeight - y(d.value);
-            })
-            .attr('y', function(d) {
-                return innerGraphHeight - (innerGraphHeight - y(d.value));
-            });
+            .duration(240);
+    return result;
+}
 
+var setBarHeightUsingData = function(bars, yScale, innerGraphHeight){
+    bars.attr('height', function(dataPoint) {
+        return innerGraphHeight - yScale(dataPoint.value);
+    })
+    .attr('y', function(dataPoint) {
+        return innerGraphHeight - (innerGraphHeight - yScale(dataPoint.value));
+    });
+}
 
-        addYaxis(svg, yAxis);
-
-        svg.append('g')
+var addXaxis = function(svg, xAxis, innerGraphHeight){
+    svg.append('g')
             .attr('class', 'x axis')
             .attr('transform', 'translate(0, ' + (innerGraphHeight) + ')')
             .call(xAxis);
+}
 
+charts.plotBarChart = function(divId, events, fromTime, tillTime, units) {
+    events = _.sortBy(events, function(e) {
+        return e.date;
+    })
+    setTimeout(function() {
+        $(divId).empty();
+        var margin = {
+            top: 20,
+            right: 0,
+            bottom: 0,
+            left: 0
+        };
+        var width = window.innerWidth;
+        var height = width / 1.61;
+        var weekAgo = new Date(moment().subtract('days', 6).format("MM/DD/YYYY"));
+        var tomorrow = new Date(moment().add('day', 1).format("MM/DD/YYYY"));
+        var xWidth = width/7;
+        var x = d3.time.scale()
+            .domain([weekAgo, tomorrow])
+            .rangeRound([0, width - margin.left - margin.right])
+            .nice();
+
+        var maxDataValue = d3.max(events, function(d) {
+            return d.value;
+        });
+
+        var innerGraphHeight = height - margin.top - margin.bottom;
+
+        var y = d3.scale.linear()
+            .domain([0, maxDataValue])
+            .range([innerGraphHeight, 0])
+            .nice(4);
+
+        var xAxis = d3.svg.axis()
+            .scale(x);
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient('right')
+            .ticks(5);
+
+        var svg = d3.select(divId).append('svg')
+            .attr('class', 'chart')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g')
+            .attr('id', 'bars')
+            .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+        
+        addGradients(svg);
+        addFilter(svg);
+
+        var chart = svg.selectAll('.chart');
+
+        var bars = chart.data(events)
+            .enter()
+            .append('rect')
+            .attr('class', 'bar');
+
+        bars.style("stroke", "rgba(255,255,255,0.7)");
+        setStartToCollapsed(bars, x, height, xWidth);
+        bars = addAnimation(bars);
+        setBarHeightUsingData(bars, y, innerGraphHeight);
+        attachClickHighlightAnimated(chart, events);
+        addYaxis(svg, yAxis);
+        addXaxis(svg, xAxis, innerGraphHeight);
         createOverlayDropshadow(svg);
         addNowLine(svg, x, innerGraphHeight);
         addUnits(svg, units, innerGraphHeight);
