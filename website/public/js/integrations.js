5eda59f859ae9046e144dbf2ddcf2f0a506cdb48
Devika Deshmukh <ddeshmukh@equalexperts.com>
2015-02-25 12:13:42 +0530

    --Opening the integration in the same window

--- website/public/js/integrations.js
+++ website/public/js/integrations.js
@@ -1,8 +1,7 @@
-$(function(){
-    var connect =  $("#connect");
-    connect.click(function(){
+$(function () {
+    var connect = $("#connect");
+    connect.click(function () {
         var url = connect.attr("url") + "?username=" + connect.attr("username") + "&token=" + connect.attr("token");
-        var win = window.open(url, '_blank');
-        win.focus();
+        window.location.href = url;
     });
 });
