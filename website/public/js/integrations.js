de1b35a0a680ca92127220b7999a01c3855a41a9
Devika Deshmukh <ddeshmukh@equalexperts.com>
2015-02-26 12:29:57 +0530

    -- Added styling for integration details page -- Added timeline link to proceed

--- website/public/js/integrations.js
+++ website/public/js/integrations.js
@@ -1,5 +1,5 @@
 $(function () {
-    var connect = $("#connect");
+    var connect = $(".connect");
     connect.click(function () {
         var url = connect.attr("url") + "?username=" + connect.attr("username") + "&token=" + connect.attr("token");
         window.location.href = url;
