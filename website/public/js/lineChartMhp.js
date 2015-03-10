d0a2e3cb465c888aa17eef63ff9227cfb266fb2e
Maximilian Holden Perchik <maximilianhp@me.com>
2015-03-10 13:32:57 +0000

    fullscreen

--- website/public/js/lineChartMhp.js
+++ website/public/js/lineChartMhp.js
@@ -1,17 +1,29 @@
 function InitChart() {
 
+    var sizeGraph = function () {
+        var windowWidth = window.innerWidth,
+            windowHeight = window.innerHeight;
+            margin = {
+                top: 50,
+                right: 50,
+                bottom: 50,
+                left: 50
+            },
+
+        width = windowWidth;
+        height = windowHeight;
+
+        innerWidth = width - margin.left - margin.right;
+        innerHeight = height - margin.top - margin.bottom;
+
+    };
+
+    sizeGraph();
+
     var weekAgo = new Date(moment().subtract('days', 6).format("MM/DD/YYYY"));
     var today = new Date(moment().format("MM/DD/YYYY"));
 
     var chart = d3.select("#chart");
-        width = 1000,
-        height = 500,
-        margin = {
-            top: 50,
-            right: 50,
-            bottom: 50,
-            left: 50
-        },
 
         xRange = d3.time.scale()
             .range([margin.left, width - margin.right])
@@ -23,9 +35,17 @@ function InitChart() {
                 return d.value;
             })]);
 
-        xAxis = d3.svg.axis().scale(xRange).tickFormat(d3.time.format('%A')).ticks(5);;
+        xAxis = d3.svg.axis()
+            .scale(xRange)
+            .tickFormat(d3.time.format('%A'))
+            .ticks(5)
+            .tickSubdivide(true);
 
-        yAxis = d3.svg.axis().scale(yRange).orient('left');
+        yAxis = d3.svg.axis()
+            .scale(yRange)
+            .orient('left')
+            .ticks(5)
+            .tickSubdivide(true);
 
         chart.append('svg:g')
             .attr('class', 'x axis')
@@ -36,5 +56,20 @@ function InitChart() {
             .attr('class', 'y axis')
             .attr('transform', 'translate(' + (margin.left) + ',0)')
             .call(yAxis);
+
+    var lineFunc = d3.svg.line()
+        .x(function(d) {
+            return xRange(d.date);
+        })
+        .y(function(d) {
+            return yRange(d.value);
+        })
+        .interpolate('linear');
+
+    chart.append('svg:path')
+        .attr('d', lineFunc(events))
+        .attr('stroke', 'blue')
+        .attr('stroke-width', 2)
+        .attr('fill', 'none');
 
 }
