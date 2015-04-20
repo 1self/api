ed53647c0b3921994a830440185ae135932172db
Devika Deshmukh <ddeshmukh@equalexperts.com>
2015-04-20 11:35:59 +0530

    -- If the user has no integration data after migration, set latestSyncFieldto default

--- migrations/003_clear_the_data_till_13th_March.js
+++ migrations/003_clear_the_data_till_13th_March.js
@@ -7,28 +7,10 @@ var platformService = require('./platformService.js');
 var appIds = ["app-id-9adce70ae3ef5c4c8389910b9abb95b1", //github
     "app-id-07be2f65eccd23895b75313a1eb8283b", //lastfm
     "app-id-tw4f3dsd91d9a3e715ff98bb9eedbd0a", //twitter
-    "app-id-gf4f3dsd93d9a3e715ff98bb9eedbd0a", //google fit
-    "app-id-fsq9954f966461936d47b7b4ba1e6f2e", //Four Square
-    "app-id-1bb5f1c77f0df722a9b1bc650a41988a", //Instagram
-    "app-id-hn80883506c93b80dcf2c3780d223eaa", //Hacker News
-    "app-id-st39ec18b3ad1dc3f3783f856c033418", //Strava
-    "app-id-rtbbd9175a69edbc4lol5d2747b638a7"]; //Rescue time
+    "app-id-gf4f3dsd93d9a3e715ff98bb9eedbd0a"]; //Google Fit
 
 var findStreamsForAppIds = function (appIds) {
-    var query = {
-        "appId": {
-            "$in": appIds
-        }
-    };
-    var projection = {
-        "streamid": 1
-    };
-    return mongoRepository.find("stream", query, projection)
-        .then(function (streams) {
-            return _.forEach(streams, function (stream) {
-                return stream.streamid;
-            });
-        });
+
 };
 
 var getLatestSyncField = function (streamId) {
@@ -49,6 +31,9 @@ var getLatestSyncField = function (streamId) {
     };
     platformService.filter(query)
         .then(function (result) {
+            if (_.isEmpty(result)) {
+                deferred.resolve();
+            }
             deferred.resolve(result[0].payload.latestSyncField);
         }, function (err) {
             deferred.reject(err);
@@ -70,22 +55,40 @@ var updateLatestSyncField = function (streamId, latestSyncField) {
     return deferred.promise;
 };
 
-findStreamsForAppIds(appIds)
-    .then(function (streamIds) {
-        _.forEach(streamIds, function(streamId){
+var query = {
+    "appId": {
+        "$in": appIds
+    }
+};
+var projection = {};
+
+mongoRepository.find("stream", query, projection)
+    .then(function (streams) {
+        _.forEach(streams, function (stream) {
             var query = {
-                "payload.streamid": streamId,
+                "payload.streamid": stream.streamid,
                 "event.createdOn": {
                     "$gt": moment("2015-03-13T00:00:00.000Z").toISOString()
                 }
             };
-            return eventRepository.remove("oneself",query)
-                .then(function(noOfEventsRemoved){
+            return eventRepository.remove("oneself", query)
+                .then(function (noOfEventsRemoved) {
                     console.log("No Of Events Removed: ", noOfEventsRemoved);
                     return getLatestSyncField(streamId);
                 })
-                .then(function(latestSyncField){
+                .then(function (latestSyncField) {
+                    if (_.isEmpty(latestSyncField)) {
+                        if (stream.appId === "app-id-tw4f3dsd91d9a3e715ff98bb9eedbd0a") {
+                            latestSyncField = "00000000";
+                        }
+                        else if(stream.appId === "app-id-gf4f3dsd93d9a3e715ff98bb9eedbd0a"){
+                            latestSyncField = null;
+                        }
+                        else {
+                            latestSyncField = new Date(1970,1,1).toISOString();
+                        }
+                    }
                     return updateLatestSyncField(streamId, latestSyncField)
                 })
-        })
+        });
     });
