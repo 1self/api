64ee95e8060b187b8e459bc72b3c249b4333af12
Ed Sykes <ed.sykes@gmail.com>
2015-07-28 14:36:36 +0100

    Added patch to the options methods

--- main.js
+++ main.js
@@ -1861,7 +1861,7 @@ app.get('/v1/app', function (req, res) {
 
 app.options('*', function (request, response) {
     response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,accept,x-requested-with,x-withio-delay');
     response.send();
 });
